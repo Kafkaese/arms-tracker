0be354f06436d8c7923837ee6f3ee9122b0697b6
Frederik Laubisch <kafkaese@gmail.com>
2023-03-08 10:52:56 +0100

    get tables from wikipedia

--- taro/data/scrapers.py
+++ taro/data/scrapers.py
@@ -1,1 +1,19 @@
 from bs4 import BeautifulSoup
+import requests as re
+
+def get_armed_conflicts():
+    res = re.get('https://en.wikipedia.org/wiki/List_of_ongoing_armed_conflicts').content
+
+    soup = BeautifulSoup(res, 'html.parser')
+
+    
+    # Tables are classed by numbe of combat related deaths
+    #table_numbers = ['10000','1000', '100']
+    
+    # First 4 tables contain the data 
+    tables =soup.find_all('table')[:3]
+
+
+    
+if __name__ == "__main__":
+    get_armed_conflicts()
