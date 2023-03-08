2717e5dfa72759df5695c1f9b1e7211638dac8de
Frederik Laubisch <kafkaese@gmail.com>
2023-03-08 11:32:00 +0100

    scrape conflict names

--- taro/data/scrapers.py
+++ taro/data/scrapers.py
@@ -13,7 +13,20 @@ def get_armed_conflicts():
     # First 4 tables contain the data 
     tables =soup.find_all('table')[:3]
 
+    conflicts = []
+    
+    #Iterate over conflict tables
+    for table in tables:
+        
+        #extract conflicts as rows. Skip header by starting at tbody (does not work)
 
+        conflict_rows = table.find('tbody').find_all('tr')
+        for conflict in conflict_rows[1:]:
+
+            conflicts.append({'name': conflict.find_all('td')[1].find('a').text})
+        
+    print(conflicts)
+    
     
 if __name__ == "__main__":
     get_armed_conflicts()
