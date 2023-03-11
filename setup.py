4a1f50bfc1b89c034599ec94a578d1c77967e93c
Frederik Laubisch <kafkaese@gmail.com>
2023-03-11 22:18:30 +0100

    add requirements

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup
 
+with open("requirements.txt") as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if "git+" not in x]
+
+
 setup(name='taro',
       version='0.0.1',
       author='Frederik Laubisch',
       author_email='kafkaese@gmail.com',
+      install_requires=requirements,
       packages='taro')
