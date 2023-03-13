5ccbb051b4dd701a0cc542c231b813f03dc53911
Frederik Laubisch <kafkaese@gmail.com>
2023-03-13 10:19:27 +0100

    fix fior url included in dict

--- taro/tests/test_conflict_scraper.py
+++ taro/tests/test_conflict_scraper.py
@@ -5,5 +5,5 @@ def test_success():
         assert get_armed_conflicts() != None, 'Scraping not successfull!'
         
 def test_keys():
-    assert list(get_armed_conflicts()[0].keys()) == ['name', 'country', 'cum_fat']
+        assert list(get_armed_conflicts()[0].keys()) == ['name', 'country', 'url', 'cum_fat']
         
