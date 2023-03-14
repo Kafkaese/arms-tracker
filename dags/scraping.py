143d525e6e1a294710423db1241e30bebaddf1f8
Frederik Laubisch <kafkaese@gmail.com>
2023-03-14 10:36:54 +0100

    check wiki available

--- dags/scraping.py
+++ dags/scraping.py
@@ -1,6 +1,7 @@
 # Airflow
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.providers.http.sensors.http import HttpSensor
 
 # Taro
 from taro.data import scrapers, database
@@ -9,6 +10,12 @@ from taro.data import scrapers, database
 from datetime import datetime
 
 with DAG('scrapers', start_date=datetime(2023, 3, 14), schedule_interval='@daily', catchup=False) as dag:
+    
+    # Check can reach website
+    is_wiki_available = HttpSensor(
+        task_id = 'is_wiki_available',
+        http_conn_id = 'https://en.wikipedia.org/wiki/List_of_ongoing_armed_conflicts' 
+    )
     
     # Run scrapers 
     
