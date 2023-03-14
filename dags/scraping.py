8e6f2dd6c3e74173a594f6af00a49933007cbf49
Frederik Laubisch <kafkaese@gmail.com>
2023-03-14 09:38:34 +0100

    set up scraping DAG

--- dags/scraping.py
+++ dags/scraping.py
@@ -3,4 +3,15 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 
 # Taro
-from taro.data import scrapers
+from taro.data import scrapers, database
+
+# Utils
+from datetime import datetime
+
+with DAG('scrapers', start_date=datetime(2023, 3, 14), schedule_interval='@daily', catchup=False) as dag:
+    
+    # Run scrapers 
+    
+    # Write data to sqlite
+    
+    
