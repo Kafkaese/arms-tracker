cf0f5494bab192f9471480e3074a8c69156c5cf6
Frederik Laubisch <kafkaese@gmail.com>
2023-03-15 14:47:34 +0100

    add store data task sekeleton to workflow

--- airflow/dags/scraping.py
+++ airflow/dags/scraping.py
@@ -9,9 +9,15 @@ from taro.data import scrapers, database
 # Utils
 from datetime import datetime
 
+# Scraper callable
 def _run_scrapers():
     conflicts = scrapers.get_armed_conflicts()
     results = [scrapers.get_conflict_belligerents(conflict['url']) for conflict in conflicts]
+
+
+# Databse callable
+def _save_data():
+    pass
 
 with DAG('scraping', start_date=datetime(2023, 3, 14), schedule='@daily', catchup=False) as dag:
     
@@ -23,12 +29,15 @@ with DAG('scraping', start_date=datetime(2023, 3, 14), schedule='@daily', catchu
     )
     
     # Run scrapers 
-    
     run_scrapers = PythonOperator(
         task_id = 'run_scrapers',
         python_callable=_run_scrapers
     )
     
     # Write data to sqlite
+    save_data = PythonOperator(
+        task_id = 'save_data',
+        python_callable = _save_data
+    )
     
-is_wiki_available >> run_scrapers 
+is_wiki_available >> run_scrapers >> save_data
