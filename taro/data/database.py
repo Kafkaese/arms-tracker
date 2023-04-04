b03bd724865f95cf9af68ff7ba5ee7c154ab6653
Frederik Laubisch <kafkaese@gmail.com>
2023-04-04 15:45:42 +0200

    restore table from BACKUP if error occurs during writing

--- taro/data/database.py
+++ taro/data/database.py
@@ -44,37 +44,58 @@ def create_write_dict_db(table_name: str, data: list, verbose = False):
         except:
             pass
         
+        try:   
+            # create field names for table from dictionary keys
+            data_types = {'int': 'INTEGER', 'float': 'REAL', 'str': 'TEXT'}
             
-        # create field names for table from dictionary keys
-        data_types = {'int': 'INTEGER', 'float': 'REAL', 'str': 'TEXT'}
+            field_names = [f'{key} {data_types[type(value).__name__]}' for key, value in data[0].items()]
+            field_names = ', '.join(field_names)
+            
+            # Create table 
+            cursor.execute(f'create table {table_name} ({field_names});') 
+            
+            ## Insert data into table
+            # Pepare keys
+            keys = ', '.join(data[0].keys())
+            
+            # prepare values as list of tuples
+            values = [ tuple([str(value) for value in data_dict.values()]) for data_dict in data ]
+            
+            # insert all 
+            cursor.executemany(f"insert into {table_name} ({keys}) VALUES ({('?, '*len(values)).strip(', ')});", values)
         
-        field_names = [f'{key} {data_types[type(value).__name__]}' for key, value in data[0].items()]
-        field_names = ', '.join(field_names)
+            # Show student table
+            if verbose:
+                cursor.execute(f'select * from {table_name};')
         
-        # Create table 
-        cursor.execute(f'create table {table_name} ({field_names});') 
+                # View result
+                result = cursor.fetchall()
+                print(result)
         
-        ## Insert data into table
-        # Pepare keys
-        keys = ', '.join(data[0].keys())
+            # Commit work and close connection
+            sqliteConnection.commit()
+            cursor.close()
         
-        # prepare values as list of tuples
-        values = [ tuple([str(value) for value in data_dict.values()]) for data_dict in data ]
-        
-        # insert all 
-        cursor.executemany(f"insert into {table_name} ({keys}) VALUES ({('?, '*len(values)).strip(', ')});", values)
-    
-        # Show student table
-        if verbose:
-            cursor.execute(f'select * from {table_name};')
-    
-            # View result
-            result = cursor.fetchall()
-            print(result)
-    
-        # Commit work and close connection
-        sqliteConnection.commit()
-        cursor.close()
+        # If error occurs during writing into table, restore with backup if exists
+        except sqlite3.Error as error:
+            print(f'ERROR: {error}')
+            listOfTables = cursor.execute(
+                f"""SELECT tbl_Name FROM sqlite_master WHERE type='table'
+                AND tbl_Name='{table_name}_BACKUP'; """).fetchall()
+            
+            try:
+                cursor.execute(f"DROP TABLE {table_name};")
+            except:
+                pass
+            
+            if listOfTables != []:
+                cursor.execute(f"CREATE TABLE {table_name} as SELECT * FROM {table_name}_BACKUP")
+                
+                cursor.execute(f"DROP TABLE {table_name}_BACKUP;")
+            
+            
+                
+            
     
     except sqlite3.Error as error:
         print('Error occurred - ', error)
