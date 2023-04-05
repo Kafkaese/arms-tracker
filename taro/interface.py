61c5e94b2d04fb23cef7d53f64ab0d3c713379c1
Frederik Laubisch <kafkaese@gmail.com>
2023-04-05 12:21:52 +0200

    remove debug line

--- taro/interface.py
+++ taro/interface.py
@@ -8,6 +8,5 @@ def run_scraper():
     # scrape belligerent information for all conflicts
     results = [get_conflict_belligerents(conflict['url']) for conflict in conflicts]
     
-    print(conflicts)
     # dump conflicts into database
     create_write_dict_db('conflicts', conflicts)
